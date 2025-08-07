bfaf73b7da208e8f5d89290f6af2251a56976f30
Kingsley Okon <kingsleyokon610@gmail.com>
2025-08-08 00:50:57 +0100

    updates

--- controller/reqlineController.js
+++ controller/reqlineController.js
@@ -1,5 +1,20 @@
 const Reqline = require('../model/reqlineModel')
 
+// Helper function
+function parseReqline(reqline) {
+  const [method, url] = reqline.trim().split(/\s+/);
+
+  if (!method || !url) {
+    throw new Error("Invalid request line format");
+  }
+
+  if (!['GET', 'POST'].includes(method.toUpperCase())) {
+    throw new Error("Invalid HTTP method. Only GET and POST are supported");
+  }
+
+  return { method: method.toUpperCase(), url };
+}
+
 
 
 const reqline = async (req, res) => {
