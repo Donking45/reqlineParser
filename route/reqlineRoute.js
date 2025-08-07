f93db4c1c72a49809a7168e5d813f06411abae02
Kingsley Okon <kingsleyokon610@gmail.com>
2025-08-08 00:18:50 +0100

    updates

--- route/reqlineRoute.js
+++ route/reqlineRoute.js
@@ -4,7 +4,7 @@ const { reqline } = require('../controller/reqlineController')
 
 
 
-router.post('./parse', reqline);
-router.get('./parse', reqline);
+router.post('/parse', reqline);
+router.get('/parse', reqline);
 
 module.exports = router;
