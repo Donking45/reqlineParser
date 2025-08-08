2473b318b37a21b21704dcbd74ba7490850469f3
Kingsley Okon <kingsleyokon610@gmail.com>
2025-08-08 10:23:23 +0100

    updates

--- controller/reqlineController.js
+++ controller/reqlineController.js
@@ -1,8 +1,10 @@
 const Reqline = require('../model/reqlineModel')
 
 
-const parseReqline = (req, res) => {
+const parseReqline = async (req, res) => {
   const { reqline } = req.body;
+
+  const startTime = Date.now();
 
   if (!reqline) {
     return res.status(400).json({ error: "Request line is required" });
@@ -14,31 +16,21 @@ const parseReqline = (req, res) => {
     return res.status(400).json({ error: "Invalid request line format" });
   }
 
-  const [method, fullUrl, httpVersion] = parts;
+  const [method, path, httpVersion] = parts;
 
   if (!['GET', 'POST'].includes(method.toUpperCase())) {
     return res.status(400).json({ error: "Invalid HTTP method. Only GET and POST are supported" });
   }
 
-  const [path, queryString] = fullUrl.split('?');
-  const query = {};
-
-  if (queryString) {
-    queryString.split('&').forEach(pair => {
-      const [key, value] = pair.split('=');
-      query[key] = decodeURIComponent(value || '');
-    });
-  }
-
-  const timestamp = Date.now();
+  const endTime = Date.now();
 
   res.json({
     method,
     path,
-    query,
     httpVersion,
-    timestamp,
-    duration: `${Date.now() - timestamp}ms`
+    startTimestamp: startTime,
+    endTimestamp: endTime,
+    duration: endTime - startTime
   });
 };
 
